8a11abf61dc3ec380e14ac8fe0ba8a1d07e0b799
iza <shigemura0820@yahoo.co.jp>
2015-04-24 13:29:40 +0900

    answer update

--- ruby_method.rb
+++ ruby_method.rb
@@ -4,11 +4,12 @@ animals = ["dog", "cat", "mouse"]
 animals1 =	["dog", "cat", "mouse","elephant"]
 scores = { 'Carol' => 90, 'Alice' => 50, 'David' => 40, 'Bob' => 60 }
 arr = []
-user1 = User.new
-user2 = User.find(3)
-user3 = user2.destroy 
+new_user = User.new
+user3 = User.find(3)
+del_user = user3.destroy 
 error = nil
-# dammy
+
+
 
 numbers.map {|item| item * 2 }
 numbers.select {|item| item % 2 == 0 }
@@ -20,26 +21,21 @@ User.find(3)
 User.find_by(username: 'カツオ')
 User.all.map(&:username)
 User.pluck(:id)
-animals.try(:size)
-error.try(:size)
-animals ||= 'hello'
+new_user.try(:size)
 error ||= 'hello'
 User.find(0).blank?
 User.find(0).present?
-numbers.empty? ? "empty" : "valid"
-arr.empty? ? "empty" : "valid"
-numbers if numbers.size > 4 
-numbers unless numbers.size > 4
-animals.index_by do |n|
-	"#{n} * 1"
-end
+numbers.size>3 ? "valid" : "invalid"
+animals if animals.size > 2
+animals unless animals.size > 2
+User.all.index_by(&:id)
 animals.each_with_index {|anim, i| puts "#{i+1}. #{anim}" }
 scores.each_with_object([]) do |(key, val), arr|
 	arr << key if val >= 60
 end
-puts animals1.any? {|word| word.size > 7 }
-puts animals.any? {|word| word.size > 7 }
-user1.new_record?
-user1.persisted?
-user2.persisted?
+numbers.any? {|num| num > 7 }
+numbers.any? {|num| num.size > 8 }
+new_user.new_record?
+new_user.persisted?
 user3.persisted?
+del_user.persisted?
